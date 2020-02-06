0fd9b9f389755d582805e8d8bc473dd47a3a791e
chixinaiya <1251512427@qq.com>
2020-02-06 11:03:15 +0800

    登录模块

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Vue from 'vue'
 import App from './App.vue'
+
+//导入路由
+//import router from './router/index.js'
+//1.router/index.js  省略 router 默认访问index.js文件
+//2. ./ ../ 去找到相应的目录并不是很准确  使用vue-cil提供的@（src路径）路径
+import router from '@/router'
 import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.use(Elementui)
+
+Vue.use(ElementUI)
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
+  render: h => h(App),
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+//导入
+import VueRouter from 'vue-router'
+//注册
+import Vue from 'vue'
+import Login from '@/views/login'
+
+Vue.use(VueRouter)
+//初始化
+const router = new VueRouter({
+
+  routes: [
+    { path: '/login', component: Login }
+  ]
+  // 路由规则
+})
+//导出
+export default router
