8dea8012dc8996dd49559859fac30b6d073afa53
chixinaiya <1251512427@qq.com>
2020-02-07 10:44:46 +0800

    首页-组件

--- src/router/index.js
+++ src/router/index.js
@@ -3,13 +3,15 @@ import VueRouter from 'vue-router'
 //注册
 import Vue from 'vue'
 import Login from '@/views/login'
+import Home from '@/views/home'
 
 Vue.use(VueRouter)
 //初始化
 const router = new VueRouter({
 
   routes: [
-    { path: '/login', component: Login }
+    { path: '/login', component: Login },
+    { path: '/', component: Home }
   ]
   // 路由规则
 })
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,14 +1,13 @@
 <template>
-  <div id="app">
-    
-    <router-view></router-view>
+  <div class="container-home">
+   home
   </div>
 </template>
 
 <script>
 
 export default {
-  name: 'app'
+  name: 'app-home'
 }
 </script>
 
