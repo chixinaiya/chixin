29580d4f34273530270ad5575e3b9a4c9d0cd16a
chixinaiya <1251512427@qq.com>
2020-02-07 14:21:15 +0800

    首页-头部导航

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,67 @@
 <template>
-  <div class="container-home">
-   home
-  </div>
+  <el-container class="container-home">
+    <el-aside class="my-aside" width="200px">Aside</el-aside>
+    <el-container>
+      <el-header class="my-header">
+        <span class="icon el-icon-s-fold"></span>
+        <span class="text">江苏传智播客科技教育有限公司</span>
+        <!-- 右边下拉菜单 -->
+        <el-dropdown class="my-dropdown">
+          <span class="el-dropdown-link">
+            <img class="head" src="../../assets/avatar.jpg" alt="">
+            <strong class="name">周杰伦</strong>
+            <i class="el-icon-arrow-down el-icon--right"></i>
+          </span>
+          <el-dropdown-menu slot="dropdown">
+            <el-dropdown-item>个人设置</el-dropdown-item>
+            <el-dropdown-item>退出登录</el-dropdown-item>
+          </el-dropdown-menu>
+        </el-dropdown>
+      </el-header>
+      <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
-
 export default {
-  name: 'app-home'
-}
+  name: "app-home"
+};
 </script>
 
 <style lang="less">
-
+.container-home {
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  left: 0;
+  top: 0;
+  .my-aside {
+    background-color: #002233;
+  }
+  .my-header {
+    border-bottom: 1px solid #ddd;
+    line-height: 60px;
+    .icon {
+      font-size: 24px;
+      vertical-align: middle;
+    }
+    .text {
+      vertical-align: middle;
+      padding-left: 10px;
+    }
+    .my-dropdown {
+      float: right;
+      .head{
+        width: 30px;
+        height: 30px;
+        vertical-align: middle;
+      }
+      .name {
+        padding-left: 5px;
+        vertical-align: middle;
+      }
+    }
+  }
+}
 </style>
