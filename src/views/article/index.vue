8d590a586b9e29df0710422c55ad3e268e2dff33
chixinaiya <1251512427@qq.com>
2020-02-11 15:09:16 +0800

    内容管理-筛选区域布局

--- src/views/article/index.vue
+++ src/views/article/index.vue
@@ -1,22 +1,72 @@
 <template>
-  <div class='container'>Article</div>
+  <div class="container-article">
+    <el-card>
+      <div slot="header">
+        <!-- 面包屑 -->
+        <el-breadcrumb separator-class="el-icon-arrow-right">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>内容管理</el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+      <!-- 表单 -->
+      <el-form label-width="80px" size="small">
+        <el-form-item label="状态">
+          <el-radio-group v-model="filterData.status">
+            <el-radio :label="null">全部</el-radio>
+            <el-radio :label="0">草稿</el-radio>
+            <el-radio :label="1">待审核</el-radio>
+            <el-radio :label="2">审核通过</el-radio>
+            <el-radio :label="3">审核失败</el-radio>
+            <el-radio :label="4">已删除</el-radio>
+          </el-radio-group>
+        </el-form-item>
+        <el-form-item label="频道">
+          <el-select v-model="filterData.channel_id" placeholder="请选择">
+            <el-option
+              v-for="item in channelOptions"
+              :key="item.value"
+              :label="item.label"
+              :value="item.value"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="日期">
+          <el-date-picker
+            v-model="dateArr"
+            type="daterange"
+            range-separator="至"
+            start-placeholder="开始日期"
+            end-placeholder="结束日期"
+          ></el-date-picker>
+        </el-form-item>
+        <el-form-item>
+          <el-button type="primary">筛选</el-button>
+        </el-form-item>
+      </el-form>
+    </el-card>
+  </div>
 </template>
 
 <script>
 export default {
   //注意；组件名称不能和原生标签重名
-  name:'app-article',
-  created (){
-    //发请求进行测试
-    this.$http.get('http://ttapi.research.itcast.cn/mp/v1_0/articles').then(res=>{
-      console.log(res.data);
-      
-    }).catch(err=>{
-      console.log(err);
-      
-    })
+  name: "app-article",
+  data() {
+    return {
+      filterData: {
+        status: null,
+        channel_id: null,
+        begin_pubdate:null,
+        end_pubdate:null
+      },
+      channelOptions: [
+        { label: "前端", value: 1 },
+        { label: "数据库", value: 2 }
+      ],
+      dateArr:[]
+    };
   }
-}
+};
 </script>
 
 <style scoped lang='less'></style>
